8909f83761d299db047099f75652f8d83c4dd857
AKrgpt99 <abhinav64@live.com>
2022-01-31 22:31:45 -0500

    Subgraph deployed with only mint and transfer functionality

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,84 +1,135 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import {
   ArtisanERC721,
-  Approval,
-  ApprovalForAll,
-  CancelListing,
-  CancelOffer,
-  Listing,
-  Mint,
-  Offer,
-  Sale,
-  Transfer
-} from "../generated/ArtisanERC721/ArtisanERC721"
-import { ExampleEntity } from "../generated/schema"
+  ArtisanERC721__asksOfResultValue0Struct,
+  ArtisanERC721__bidsOfResultValue0Struct,
+  CancelListing as CancelListingEvent,
+  CancelOffer as CancelOfferEvent,
+  Listing as ListingEvent,
+  Mint as MintEvent,
+  Offer as OfferEvent,
+  Sale as SaleEvent,
+  Transfer as TransferEvent,
+} from "../generated/ArtisanERC721/ArtisanERC721";
+import { Token, User, Bid, Ask } from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.artistOf(...)
-  // - contract.asksOf(...)
-  // - contract.balanceOf(...)
-  // - contract.bidsOf(...)
-  // - contract.categories(...)
-  // - contract.categoryOf(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
+function createBids(tokenContract: ArtisanERC721, tokenId: BigInt): void {
+  tokenContract
+    .bidsOf(tokenId)
+    .map<string>((bid: ArtisanERC721__bidsOfResultValue0Struct): string => {
+      const newBid = new Bid(
+        bid.bidder.toHexString() + bid.tokenId.toString() + bid.price.toString()
+      );
+      newBid.token = bid.tokenId.toString();
+      newBid.bidder = bid.bidder.toHexString();
+      newBid.save();
+      return newBid.id;
+    });
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+function createAsks(tokenContract: ArtisanERC721, tokenId: BigInt): void {
+  tokenContract
+    .asksOf(tokenId)
+    .map<string>((ask: ArtisanERC721__asksOfResultValue0Struct): string => {
+      const newAsk = new Ask(
+        ask.asker.toHexString() + ask.tokenId.toString() + ask.price.toString()
+      );
+      newAsk.token = ask.tokenId.toString();
+      newAsk.asker = ask.asker.toHexString();
+      newAsk.save();
+      return newAsk.id;
+    });
+}
 
-export function handleCancelListing(event: CancelListing): void {}
+export function handleCancelListing(event: CancelListingEvent): void {}
 
-export function handleCancelOffer(event: CancelOffer): void {}
+export function handleCancelOffer(event: CancelOfferEvent): void {}
 
-export function handleListing(event: Listing): void {}
+export function handleListing(event: ListingEvent): void {}
 
-export function handleMint(event: Mint): void {}
+export function handleMint(event: MintEvent): void {
+  let address = event.address;
+  let tokenId: BigInt;
 
-export function handleOffer(event: Offer): void {}
+  tokenId = event.params.token.tokenId;
 
-export function handleSale(event: Sale): void {}
+  let token = Token.load(tokenId.toString());
 
-export function handleTransfer(event: Transfer): void {}
+  if (!token) {
+    token = new Token(tokenId.toString());
+    let tokenContract = ArtisanERC721.bind(address);
+    token.uri = tokenContract.tokenURI(tokenId);
+    token.owner = tokenContract.ownerOf(tokenId).toHexString();
+    token.artist = tokenContract.artistOf(tokenId).toHexString();
+
+    token.listed = event.params.token.listed;
+
+    createBids(tokenContract, tokenId);
+    createAsks(tokenContract, tokenId);
+
+    token.save();
+  }
+
+  // Owner
+  let user = User.load(event.params.token.owner.toHexString());
+
+  if (!user) {
+    user = new User(event.params.token.owner.toHexString());
+    user.address = event.params.token.owner;
+    user.save();
+  }
+
+  // Artist
+  user = User.load(event.params.token.artist.toHexString());
+
+  if (!user) {
+    user = new User(event.params.token.artist.toHexString());
+    user.address = event.params.token.artist;
+    user.save();
+  }
+}
+
+export function handleOffer(event: OfferEvent): void {}
+
+export function handleSale(event: SaleEvent): void {}
+
+export function handleTransfer(event: TransferEvent): void {
+  let address = event.address;
+  let tokenId: BigInt;
+
+  tokenId = event.params.tokenId;
+
+  let token = Token.load(tokenId.toString());
+
+  if (!token) {
+    token = new Token(tokenId.toString());
+    let tokenContract = ArtisanERC721.bind(address);
+    token.uri = tokenContract.tokenURI(tokenId);
+    token.owner = tokenContract.ownerOf(tokenId).toHexString();
+    token.artist = tokenContract.artistOf(tokenId).toHexString();
+    token.listed = false;
+
+    createBids(tokenContract, tokenId);
+    createAsks(tokenContract, tokenId);
+
+    token.save();
+  }
+
+  // Buyer
+  let user = User.load(event.params.to.toHexString());
+
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.address = event.params.to;
+    user.save();
+  }
+
+  // Seller
+  user = User.load(event.params.from.toHexString());
+
+  if (!user) {
+    user = new User(event.params.from.toHexString());
+    user.address = event.params.from;
+    user.save();
+  }
+}
